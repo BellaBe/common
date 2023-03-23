956dd7c7cb3810b0f091d7b28254b3ac96dd4d0a
bellabe <bella.belgarokova@hotmail.com>
2023-03-23 14:18:32 +0300

    add env config functions

---
+++ src/utils/config.ts
@@ -0,0 +1,52 @@
+
+export function getEnvVarAsString(
+    key: keyof NodeJS.ProcessEnv,
+): string {
+    const value = process.env[key];
+
+    if (value === undefined) {
+        const message = `The environment variable "${key}" cannot be "undefined".`;
+        throw new Error(message);
+    }
+    return value;
+}
+
+export function geEnvVarAsNumber(
+    key: keyof NodeJS.ProcessEnv,
+): number {
+    const stringValue = getEnvVarAsString(key);
+
+    const numberValue = parseFloat(stringValue);
+
+    if (Number.isNaN(numberValue)) {
+        const message = `The environment variable "${key}" has to hold a stringified number value - not ${stringValue}`;
+
+        throw new Error(message);
+    }
+
+    return numberValue;
+}
+
+export function getEnvVarAsInteger(
+    key: keyof NodeJS.ProcessEnv,
+): number {
+    const stringValue = getEnvVarAsString(key);
+
+    const numberValue = parseInt(stringValue, 10);
+
+    if (Number.isNaN(numberValue)) {
+        const message = `The environment variable "${key}" has to hold a stringified integer value - not ${stringValue}`;
+
+        throw new Error(message);
+    }
+
+    return numberValue;
+}
+
+export function extractBooleanEnvVar(
+    key: keyof NodeJS.ProcessEnv,
+): boolean {
+    const value = getEnvVarAsInteger(key);
+
+    return Boolean(value);
+}
