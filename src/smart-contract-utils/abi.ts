412efcd58bcbd3ed2528d34743b6ef2422ae336b
bellabe <bella.belgarokova@hotmail.com>
2023-03-24 17:13:39 +0300

    adjust return value

--- src/smart-contract-utils/abi.ts
+++ src/smart-contract-utils/abi.ts
@@ -6,7 +6,7 @@ export const getContractABI = async (
     params: GetContractABIParams
 ): Promise<string> => {
     const { explorer_base_url, explorer_api_key, contract_address } = params;
-    const { data: { result: abi } } = await axios.get(
+    const { data: { data: {result: abi} } } = await axios.get(
         `${explorer_base_url}?module=contract&action=getabi&address=${contract_address}&apikey=${explorer_api_key}`
     )
     return abi;
